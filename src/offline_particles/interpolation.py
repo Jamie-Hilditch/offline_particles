ba1f6f12134bdcaf6ce9f86498784bf86b88effe
Jamie Hilditch <hilditch@stanford.edu>
2025-12-03 20:54:18 -0800

    linear interpolation

---
+++ src/offline_particles/interpolation.py
@@ -0,0 +1,97 @@
+"""Submodule defining interpolation routines for field data."""
+
+import numba
+import numpy.typing as npt
+
+from .kernel_tools import split_index
+
+
+@numba.njit(nogil=True, fastmath=True)
+def linear_interpolation(idx0: float, array: npt.NDArray[float]) -> float:
+    """Perform linear interpolation on a 1D array.
+    
+    Parameters:
+        idx: The floating-point index to interpolate at.
+        array: 1D array of values to interpolate within.
+    
+    Returns:
+        The interpolated value as a float.
+    """
+    I0, f0 = split_index(idx0)
+    g0 = 1.0 - f0
+
+    v0 = array[I0]
+    v1 = array[I0 + 1]
+
+    return g0 * v0 + f0 * v1
+
+@numba.njit(nogil=True, fastmath=True)
+def bilinear_interpolation(idx0: float, idx1: float, array: npt.NDArray[float]) -> float:
+    """Perform bilinear interpolation on a 2D array.
+    
+    Parameters:
+        idx0: The floating-point index in the first dimension.
+        idx1: The floating-point index in the second dimension.
+        array: 2D array of values to interpolate within.
+    
+    Returns:
+        The interpolated value as a float.
+    """
+    I0, f0 = split_index(idx0)
+    I1, f1 = split_index(idx1)
+    g0 = 1.0 - f0
+    g1 = 1.0 - f1
+
+    v00 = array[I0, I1]
+    v01 = array[I0, I1 + 1]
+    v10 = array[I0 + 1, I1]
+    v11 = array[I0 + 1, I1 + 1]
+
+    return (
+        g0 * g1 * v00 +
+        g0 * f1 * v01 +
+        f0 * g1 * v10 +
+        f0 * f1 * v11
+    )
+
+@numba.njit(nogil=True, fastmath=True)
+def trilinear_interpolation(
+    idx0: float, idx1: float, idx2: float, array: npt.NDArray[float]
+) -> float:
+    """Perform trilinear interpolation on a 3D array.
+    
+    Parameters:
+        idx0: The floating-point index in the first dimension.
+        idx1: The floating-point index in the second dimension.
+        idx2: The floating-point index in the third dimension.
+        array: 3D array of values to interpolate within.
+    
+    Returns:
+        The interpolated value as a float.
+    """
+    I0, f0 = split_index(idx0)
+    I1, f1 = split_index(idx1)
+    I2, f2 = split_index(idx2)
+    g0 = 1.0 - f0
+    g1 = 1.0 - f1
+    g2 = 1.0 - f2
+
+    v000 = array[I0, I1, I2]
+    v001 = array[I0, I1, I2 + 1]
+    v010 = array[I0, I1 + 1, I2]
+    v011 = array[I0, I1 + 1, I2 + 1]
+    v100 = array[I0 + 1, I1, I2]
+    v101 = array[I0 + 1, I1, I2 + 1]
+    v110 = array[I0 + 1, I1 + 1, I2]
+    v111 = array[I0 + 1, I1 + 1, I2 + 1]
+
+    return (
+        g0 * g1 * g2 * v000 +
+        g0 * g1 * f2 * v001 +
+        g0 * f1 * g2 * v010 +
+        g0 * f1 * f2 * v011 +
+        f0 * g1 * g2 * v100 +
+        f0 * g1 * f2 * v101 +
+        f0 * f1 * g2 * v110 +
+        f0 * f1 * f2 * v111
+    )
--- src/offline_particles/kernel_tools.py
+++ src/offline_particles/kernel_tools.py
@@ -1,8 +1,12 @@
 """A submodule with utility functions for kernel operations."""
 
+from numbers import Number
+
 import numba
 import numpy as np
 import numpy.typing as npt
+
+from .kernel_data import KernelData
 
 
 @numba.njit(nogil=True, fastmath=True)
@@ -25,3 +29,60 @@ def unsafe_inverse_linear_interpolation(array: npt.NDArray, value: float) -> flo
     x1 = array[idx + 1]
     fraction = (value - x0) / (x1 - x0)
     return idx + fraction
+
+@numba.njit(nogil=True, fastmath=True)
+def unwrap_scalar(kernel_data: KernelData) -> Number:
+    """Unwrap a scalar kernel value from KernelData.
+
+    Parameters:
+        kernel_data: KernelData object containing the kernel values.
+
+    Returns:
+        The scalar kernel value.
+
+    Raises:
+        ValueError: If the kernel data does not contain exactly one value.
+    """
+    return kernel_data.array[()]
+
+@numba.njit(nogil=True, fastmath=True)
+def offset_indices(particle: npt.NDArray, kernel_data: KernelData) -> npt.NDArray[float]:
+    """Compute offset indices for a particle based on kernel data.
+    
+    Parameters:
+        particle: numpy scalar containing particle data.
+        kernel_data: KernelData.
+
+    Returns:
+        A tuple of numpy scalars representing the offset indices.
+    """
+    dmask = kernel_data.dmask
+    offsets = kernel_data.offsets
+    particle_index_names = ("zidx", "yidx", "xidx")
+
+    M = len(offsets)
+    out = np.empty(M, dtype=np.float64)
+    m = 0
+
+    for i in range(3):
+        if dmask[i] == 0:
+            continue 
+        out[m] = particle[particle_index_names[i]] + offsets[m]
+        m += 1
+
+    return tuple(out)
+
+@numba.njit(nogil=True, fastmath=True)
+def split_index(idx: float) -> tuple[int, float]:
+    """Split a single index into its integer and fractional parts.
+
+    Parameters:
+        idx: The index to split.
+    Returns:
+        A tuple containing the integer part and the fractional part.
+
+    """
+    int_idx = np.floor(idx)
+    frac_idx = idx - int_idx
+    return int_idx.astype(np.int32), frac_idx
+    
