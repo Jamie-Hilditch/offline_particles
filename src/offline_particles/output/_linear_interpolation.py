0bf083c984b160a55e80a4a1d4ddd4d32593b22d
Jamie Hilditch <hilditch@stanford.edu>
2026-01-01 11:28:35 -0800

    refactor linearly interpolated output to match new format

--- src/offline_particles/output/_linear_interpolation.py
+++ src/offline_particles/output/_linear_interpolation.py
@@ -18,14 +18,14 @@ DMASK_DIM_MAPPING_2D = {
 def linearly_interpolate_fields(
     fieldset: Fieldset,
     *variables: str,
-    temporary_array_prefix: str = "_output",
+    particle_field_prefix: str = "_output",
 ) -> list[Output]:
     """Output variables that linearly interpolate field data.
 
     Args:
         fieldset: The fieldset containing the fields to interpolate.
         variables: The list of variable names to interpolate.
-        temporary_array_prefix: The prefix for temporary arrays to store the output data on particles.
+        particle_field_prefix: The prefix for the particle array to store the output data.
     """
     dims = ("z", "y", "x")
     outputs = []
@@ -38,19 +38,19 @@ def linearly_interpolate_fields(
         dmask = field.dmask
         ndim = field.nspatial_dims
         dtype = field.dtype
-        tmp_name = f"{temporary_array_prefix}_{dtype}"
+        particle_field = f"{particle_field_prefix}_{dtype}"
 
         if ndim == 1:
             dim = dims[dmask.index(True)]
-            kernel = linear_interpolation_kernel(var, tmp_name, dim)
+            kernel = linear_interpolation_kernel(var, particle_field, dim)
         elif ndim == 2:
             dim = DMASK_DIM_MAPPING_2D[dmask]
-            kernel = bilinear_interpolation_kernel(var, tmp_name, dim)
+            kernel = bilinear_interpolation_kernel(var, particle_field, dim)
         elif ndim == 3:
-            kernel = trilinear_interpolation_kernel(var, tmp_name)
+            kernel = trilinear_interpolation_kernel(var, particle_field)
         else:
             raise ValueError(f"Field '{var}' has unsupported number of dimensions: {ndim}")
 
-        outputs.append(Output(var, tmp_name, kernel))
+        outputs.append(Output(var, kernel, particle_field=particle_field))
 
     return outputs
