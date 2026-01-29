4b9ebc2256b4b8dee2d89c7ea1f13d8f10e887e4
Jamie Hilditch <hilditch@stanford.edu>
2026-01-28 21:02:31 -0800

    corrrect index naming

--- src/offline_particles/kernels/common_inputs.py
+++ src/offline_particles/kernels/common_inputs.py
@@ -6,9 +6,9 @@ from ._kernels import ParticlePropertyDeclaration, ScalarDeclaration
 
 # particle properties
 STATUS_DECLARATION = ParticlePropertyDeclaration("status", np.uint8)
-ZIDX_DECLARATION = ParticlePropertyDeclaration("z_idx", np.float64)
-YIDX_DECLARATION = ParticlePropertyDeclaration("y_idx", np.float64)
-XIDX_DECLARATION = ParticlePropertyDeclaration("x_idx", np.float64)
+ZIDX_DECLARATION = ParticlePropertyDeclaration("zidx", np.float64)
+YIDX_DECLARATION = ParticlePropertyDeclaration("yidx", np.float64)
+XIDX_DECLARATION = ParticlePropertyDeclaration("xidx", np.float64)
 
 # scalars
 DT_DECLARATION = ScalarDeclaration("_dt", np.float64)
