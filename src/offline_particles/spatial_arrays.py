46c3b9e3f5ecb3a9c1446e2968339faeaf7332a7
Jamie Hilditch <hilditch@stanford.edu>
2026-05-26 11:11:16 -0700

    Add dunder methods to ArrayLayout

--- src/offline_particles/spatial_arrays.py
+++ src/offline_particles/spatial_arrays.py
@@ -169,6 +169,25 @@ class ArrayLayout:
     def __delattr__(self, name):
         raise AttributeError("ArrayLayout is immutable")
 
+    def __hash__(self):
+        return hash((self.axes, self.staggers))
+
+    def __eq__(self, other):
+        if not isinstance(other, ArrayLayout):
+            return NotImplemented
+        return self.axes == other.axes and self.staggers == other.staggers
+
+    def __repr__(self):
+        return f"ArrayLayout(axes={self.axes}, staggers={self.staggers})"
+
+    def __str__(self):
+        layout_string = "Layout("
+        layout_string += ", ".join(
+            f"{axis.value} -> {stagger.value}" for axis, stagger in zip(self.axes, self.staggers)
+        )
+        layout_string += ")"
+        return layout_string
+
 
 @dataclasses.dataclass(frozen=True, slots=True)
 class BBox:
